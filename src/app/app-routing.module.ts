6e4476e7dfe8e7aff276a2143244c93691bf7a8d
Michal Beczek (Contractor) <michal_beczek@epam.com>
2021-10-28 15:49:50 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { SubcardComponent } from './subcard/subcard.component';
+const routes: Routes = [
+  {path: 'main' , component: SubcardComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
+  
 })
+
 export class AppRoutingModule { }
---
+++ src/app/subcard/subcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subcard',
+  templateUrl: './subcard.component.html',
+  styleUrls: ['./subcard.component.css']
+})
+export class SubcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
