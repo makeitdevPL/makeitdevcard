e1f234f46f3240b7949094ce1ce6281af800a587
Michal Beczek (Contractor) <michal_beczek@epam.com>
2021-10-29 10:14:07 +0200

    first commit

--- src/app/services/currency/currency.service.ts
+++ src/app/services/currency/currency.service.ts
@@ -11,6 +11,6 @@ export class CurrencyService {
   constructor(private http : HttpClient) { }
 
   getRepos(): Observable<any> {
-    return this.http.get('http://api.nbp.pl/api/exchangerates/tables/C/')
+    return this.http.get('https://api.nbp.pl/api/exchangerates/tables/C/')
   }
 }
